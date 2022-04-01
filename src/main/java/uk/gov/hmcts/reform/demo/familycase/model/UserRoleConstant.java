578fc50b73dbcf36d71fdd2ef8cc19b5ef5a3393
RonanCog <ronan.behan@HMCTS.NET>
2022-04-01 17:14:45 +0100

    Implementation of the Java Pojos and Access

---
+++ src/main/java/uk/gov/hmcts/reform/demo/familycase/model/UserRoleConstant.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.demo.familycase.model;
+
+public interface UserRoleConstant {
+
+    final String FAMILY_GENERIC = "caseworker-family";
+    final String CASE_WORKER = "caseworker-family-caseworker";
+    final String COURT_ADMIN = "caseworker-adoption-courtadmin";
+    final String LEGAL_ADVISOR = "caseworker-family-la";
+    final String DISTRICT_JUDGE = "caseworker-family-judge";
+    final String SUPER_USER = "caseworker-family-superuser";
+    final String SOLICITOR = "caseworker-family-solicitor";
+    final String CITIZEN = "citizen";
+    final String CREATOR = "[CREATOR]";
+
+
+
+    final String CASE_TYPE_PERMISSIONS_CRU = "CRU";
+    final String CASE_TYPE_PERMISSIONS_CRUD = "CRUD";
+
+
+}
---
+++ src/main/java/uk/gov/hmcts/reform/demo/familycase/model/access/CollectionAccess.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.demo.familycase.model.access;
+
+import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
+import uk.gov.hmcts.ccd.sdk.api.HasRole;
+import uk.gov.hmcts.ccd.sdk.api.Permission;
+
+import static uk.gov.hmcts.reform.demo.familycase.model.UserRole.CASE_WORKER;
+
+
+public class CollectionAccess implements HasAccessControl {
+    @Override
+    public SetMultimap<HasRole, Permission> getGrants() {
+        SetMultimap<HasRole, Permission> grants = HashMultimap.create();
+        grants.putAll(CASE_WORKER, Permissions.READ);
+        grants.putAll(COURT_ADMIN, Permissions.READ);
+        grants.putAll(SOLICITOR, Permissions.READ);
+        grants.putAll(SUPER_USER, Permissions.READ);
+        grants.putAll(LEGAL_ADVISOR, Permissions.READ);
+        grants.putAll(DISTRICT_JUDGE, Permissions.READ);
+        grants.putAll(CREATOR, Permissions.CREATE_READ_UPDATE);
+        grants.putAll(CITIZEN, Permissions.CREATE_READ_UPDATE_DELETE);//TODO remove delete access for citizen 7th Jan 2022
+
+        return grants;
+    }
+}
+
