cf40559d1dd0dd29ef4e338f7a655bb3929867fc
Adetola Opanuga <2109031@cognizant.com>
2022-06-14 14:17:35 +0100

    Added tests for exceptions and handlers

--- src/test/java/uk/gov/hmcts/reform/cosapi/util/TestResourceUtil.java
+++ src/test/java/uk/gov/hmcts/reform/cosapi/util/TestResourceUtil.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.cosapi.util;
 
 import org.springframework.util.ResourceUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,13 +10,16 @@ public final class TestResourceUtil {
 
     private TestResourceUtil() {
     }
+
     public static String expectedResponse(final String resourcePath) throws IOException {
         return resourceAsString(resourcePath);
     }
+
     public static String resourceAsString(final String resourcePath) throws IOException {
         final File file = ResourceUtils.getFile(resourcePath);
         return new String(Files.readAllBytes(file.toPath()));
     }
+
     public static byte[] resourceAsBytes(final String resourcePath) throws IOException {
         final File file = ResourceUtils.getFile(resourcePath);
         return Files.readAllBytes(file.toPath());
