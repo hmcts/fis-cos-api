7a76f6ed7252eb7820dbb66c2a05ae770bbb11f8
RonanCog <ronan.behan@HMCTS.NET>
2022-06-14 14:11:10 +0100

    ISDB/feature-250-2 pushing create case event changes to branch feature/ISDB-250

--- src/test/java/uk/gov/hmcts/reform/cosapi/edgecase/event/privatelaw/SubmitCaseEventTest.java
+++ src/test/java/uk/gov/hmcts/reform/cosapi/edgecase/event/privatelaw/SubmitCaseEventTest.java
@@ -11,24 +11,24 @@ import uk.gov.hmcts.reform.cosapi.edgecase.model.State;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.reform.cosapi.constants.CommonConstants.SUBMIT_CASE_EVENT_ID;
-import static uk.gov.hmcts.reform.cosapi.util.ConfigTestUtil.createCaseDataConfigBuilder;
-import static uk.gov.hmcts.reform.cosapi.util.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.reform.cosapi.edgecase.event.privatelaw.SubmitCaseEvent.CITIZEN_SUBMIT;
+import static uk.gov.hmcts.reform.cosapi.testUtil.ConfigTestUtil.createCaseDataConfigBuilder;
+import static uk.gov.hmcts.reform.cosapi.testUtil.ConfigTestUtil.getEventsFrom;
 
 @ExtendWith(MockitoExtension.class)
-class SubmitCaseEventTest {
+public class SubmitCaseEventTest {
 
     @InjectMocks
     private SubmitCaseEvent submitCaseEvent;
 
     @Test
-    void shouldAddConfigurationToConfigBuilder() throws Exception {
+    void shouldAddConfigurationToConfigBuilder() {
         final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
 
         submitCaseEvent.configure(configBuilder);
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(SUBMIT_CASE_EVENT_ID);
+            .contains(CITIZEN_SUBMIT);
     }
 }
