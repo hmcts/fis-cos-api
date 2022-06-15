21e1301cc887f7cf6f58d77937c94202564166d5
Adetola Opanuga <2109031@cognizant.com>
2022-06-15 13:42:37 +0100

    ISDB-250 Fixed errors to do with events

--- src/test/java/uk/gov/hmcts/reform/cosapi/edgecase/event/privatelaw/UpdateCaseEventTest.java
+++ src/test/java/uk/gov/hmcts/reform/cosapi/edgecase/event/privatelaw/UpdateCaseEventTest.java
@@ -11,7 +11,7 @@ import uk.gov.hmcts.reform.cosapi.edgecase.model.State;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.reform.cosapi.edgecase.event.privatelaw.UpdateCaseEvent.CITIZEN_UPDATE;
+import static uk.gov.hmcts.reform.cosapi.constants.CommonConstants.UPDATE_CASE_EVENT_ID;
 import static uk.gov.hmcts.reform.cosapi.util.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.reform.cosapi.util.ConfigTestUtil.getEventsFrom;
 
@@ -30,6 +30,6 @@ public class UpdateCaseEventTest {
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(CITIZEN_UPDATE);
+            .contains(UPDATE_CASE_EVENT_ID);
     }
 }
