82e3dd43a83afbd2956ba82883617ffa48b8c595
Adetola Opanuga <2109031@cognizant.com>
2022-06-15 13:42:37 +0100

    ISDB-250 Fixed errors to do with events

--- src/test/java/uk/gov/hmcts/reform/cosapi/edgecase/event/privatelaw/SubmitCaseEventTest.java
+++ src/test/java/uk/gov/hmcts/reform/cosapi/edgecase/event/privatelaw/SubmitCaseEventTest.java
@@ -11,7 +11,7 @@ import uk.gov.hmcts.reform.cosapi.edgecase.model.State;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.reform.cosapi.edgecase.event.privatelaw.SubmitCaseEvent.CITIZEN_SUBMIT;
+import static uk.gov.hmcts.reform.cosapi.constants.CommonConstants.SUBMIT_CASE_EVENT_ID;
 import static uk.gov.hmcts.reform.cosapi.util.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.reform.cosapi.util.ConfigTestUtil.getEventsFrom;
 
@@ -23,13 +23,13 @@ public class SubmitCaseEventTest {
     private SubmitCaseEvent submitCaseEvent;
 
     @Test
-    void shouldAddConfigurationToConfigBuilder() {
+    void shouldAddConfigurationToConfigBuilder() throws Exception {
         final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
 
         submitCaseEvent.configure(configBuilder);
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(CITIZEN_SUBMIT);
+            .contains(SUBMIT_CASE_EVENT_ID);
     }
 }
