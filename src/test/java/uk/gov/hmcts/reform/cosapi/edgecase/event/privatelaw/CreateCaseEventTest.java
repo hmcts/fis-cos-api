17dd766e6a4dcf261f12d32a276fa0c9cce42c14
Adetola Opanuga <2109031@cognizant.com>
2022-06-15 13:42:37 +0100

    ISDB-250 Fixed errors to do with events

--- src/test/java/uk/gov/hmcts/reform/cosapi/edgecase/event/privatelaw/CreateCaseEventTest.java
+++ src/test/java/uk/gov/hmcts/reform/cosapi/edgecase/event/privatelaw/CreateCaseEventTest.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.cosapi.edgecase.event.privatelaw;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.collect.SetMultimap;
 import org.junit.Before;
@@ -14,33 +12,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.cosapi.common.AddSystemUpdateRole;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.CaseData;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.State;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole;
 
-import static org.mockito.Mockito.verify;
-import static uk.gov.hmcts.ccd.sdk.api.Permission.C;
-import static uk.gov.hmcts.ccd.sdk.api.Permission.R;
-import static uk.gov.hmcts.ccd.sdk.api.Permission.U;
-import static org.mockito.Mockito.when;
-
-import uk.gov.hmcts.ccd.sdk.api.Event;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyList;
-import static uk.gov.hmcts.reform.cosapi.edgecase.event.privatelaw.CreateCaseEvent.CITIZEN_CREATE;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.ccd.sdk.api.Permission.C;
+import static uk.gov.hmcts.reform.cosapi.constants.CommonConstants.CREATE_CASE_EVENT_ID;
+import static uk.gov.hmcts.ccd.sdk.api.Permission.R;
+import static uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.ccd.sdk.api.Permission.U;
 import static uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole.CITIZEN;
-import static uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole.SYSTEMUPDATE;
 import static uk.gov.hmcts.reform.cosapi.util.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.reform.cosapi.util.ConfigTestUtil.getEventsFrom;
-import static uk.gov.hmcts.reform.cosapi.util.TestFileUtil.loadJson;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -71,7 +62,7 @@ public class CreateCaseEventTest {
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(CITIZEN_CREATE);
+            .contains(CREATE_CASE_EVENT_ID);
 
         SetMultimap<UserRole, Permission> expectedRolesAndPermissions =
             ImmutableSetMultimap.<UserRole, Permission>builder()
@@ -86,48 +77,30 @@ public class CreateCaseEventTest {
     }
 
     @Test
-    void shouldSetPermissionForCitizenAndSystemUpdateRoleWhenEnvironmentIsAat() {
+    void shouldSetPermissionForCitizenAndCaseWorkerRoleWhenEnvironmentIsAat() {
         final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
 
         when(addSystemUpdateRole.addIfConfiguredForEnvironment(anyList()))
-            .thenReturn(List.of(CITIZEN, SYSTEMUPDATE));
+            .thenReturn((List.of(CITIZEN, CASE_WORKER)));
 
         createCaseEvent.configure(configBuilder);
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(CITIZEN_CREATE);
+            .contains(CREATE_CASE_EVENT_ID);
 
         SetMultimap<UserRole, Permission> expectedRolesAndPermissions =
             ImmutableSetMultimap.<UserRole, Permission>builder()
             .put(CITIZEN, C)
             .put(CITIZEN, R)
             .put(CITIZEN, U)
-            .put(SYSTEMUPDATE, C)
-            .put(SYSTEMUPDATE, R)
-            .put(SYSTEMUPDATE, U)
+            .put(CASE_WORKER, C)
+            .put(CASE_WORKER, R)
+            .put(CASE_WORKER, U)
             .build();
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getGrants)
             .containsExactlyInAnyOrder(expectedRolesAndPermissions);
-
-        verify(addSystemUpdateRole).addIfConfiguredForEnvironment(anyList());
     }
-
-    @Test
-    void shouldSetFormattedCaseReferenceWhenAboutToSubmitCallbackIsTriggered() throws Exception {
-        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        String caseDataJson = loadJson("C100CaseData.json");
-        CaseData caseData = mapper.readValue(caseDataJson, CaseData.class);
-
-        final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
-        final CaseDetails<CaseData, State> details = new CaseDetails<>();
-        details.setData(caseData);
-
-        AboutToStartOrSubmitResponse<CaseData, State> response = createCaseEvent.aboutToSubmit(details, beforeDetails);
-
-        assertThat(response.getData().getNamedApplicant()).isEqualTo(caseData.getNamedApplicant());
-    }
-
 }
