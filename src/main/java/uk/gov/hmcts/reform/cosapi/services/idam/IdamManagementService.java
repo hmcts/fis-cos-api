63ee0bd03ad9d22e3e208dab70f26d9ee2705a6a
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-06-16 13:32:19 +0100

    ISDB-258: Fetch user information

--- src/main/java/uk/gov/hmcts/reform/cosapi/services/idam/IdamManagementService.java
+++ src/main/java/uk/gov/hmcts/reform/cosapi/services/idam/IdamManagementService.java
@@ -1,15 +1,11 @@
 package uk.gov.hmcts.reform.cosapi.services.idam;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.cosapi.idam.IdamService;
 import uk.gov.hmcts.reform.cosapi.model.idam.User;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Service
 public class IdamManagementService {
 
@@ -17,7 +13,10 @@ public class IdamManagementService {
     private IdamService idamService;
 
     public User getUserDetails(String authorization) {
-        UserDetails userDetails = idamService.retrieveUser(authorization).getUserDetails();
+        uk.gov.hmcts.reform.idam.client.models.User user = idamService.retrieveUser(
+            authorization);
+
+        UserDetails userDetails = user.getUserDetails();
         return User.builder()
             .firstName(userDetails.getForename())
             .lastName(userDetails.getSurname().get())
