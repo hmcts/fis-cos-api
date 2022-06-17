e1f7da2657a8233e52d281acf236881155e4681c
Adetola Opanuga <2109031@cognizant.com>
2022-06-17 16:51:16 +0100

    ISDB-250 Further technical review changes

--- src/test/java/uk/gov/hmcts/reform/cosapi/util/TestFileUtil.java
+++ src/test/java/uk/gov/hmcts/reform/cosapi/util/TestFileUtil.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.reform.cosapi.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import uk.gov.hmcts.reform.cosapi.exception.InvalidResourceException;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -17,14 +20,16 @@ public class TestFileUtil {
         return new String(loadResource(filePath), Charset.forName("utf-8"));
     }
 
-    public static byte[] loadResource(final String filePath) throws Exception {
-        URL url = TestFileUtil.class.getClassLoader().getResource(filePath);
-
-        if (url == null) {
-            throw new IllegalArgumentException(String.format("Could not find resource in path %s", filePath));
+    public static byte[] loadResource(final String filePath) throws IOException {
+        try {
+            URL url = TestFileUtil.class.getClassLoader().getResource(filePath);
+            if (url == null) {
+                throw new IllegalArgumentException(String.format("Could not find resource in path %s", filePath));
+            }
+            return Files.readAllBytes(Paths.get(url.toURI()));
+        } catch (IOException | URISyntaxException | IllegalArgumentException ioException) {
+            throw new InvalidResourceException("Could not find resource in path " + filePath, ioException);
         }
-
-        return Files.readAllBytes(Paths.get(url.toURI()));
     }
 
     public static <T> T loadJsonToObject(String filePath, Class<T> type) {
