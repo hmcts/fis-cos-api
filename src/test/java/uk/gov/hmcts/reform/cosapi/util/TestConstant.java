54434603669e6587e4d8a8385608d8b5ed67d9e7
Adetola Opanuga <2109031@cognizant.com>
2022-06-20 10:50:21 +0100

    ISDB-250 Added DocmosisTemplateProvider, SystemUserService and further Tests

---
+++ src/test/java/uk/gov/hmcts/reform/cosapi/util/TestConstant.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.cosapi.util;
+
+public final class TestConstant {
+
+    public static final String ENGLISH_TEMPLATE_ID = "edgecaseapplication";
+    public static final String WELSH_TEMPLATE_ID = "EDG-C100-WEL-00001.docx";
+    public static final String CASE_DATA_FILE_C100 = "C100CaseData.json";
+    public static final String CASE_TEST_AUTHORISATION = "testAuth";
+
+    private TestConstant() {
+
+    }
+}
