c3ba27e616c9406b496635aa08a9fbe3660f4aa4
Adetola Opanuga <2109031@cognizant.com>
2022-06-20 17:53:20 +0100

    ISDB-250 Removed unused details from application info

--- src/test/java/uk/gov/hmcts/reform/cosapi/util/TestConstant.java
+++ src/test/java/uk/gov/hmcts/reform/cosapi/util/TestConstant.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.cosapi.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 public final class TestConstant {
 
     public static final String ENGLISH_TEMPLATE_ID = "edgecaseapplication";
     public static final String WELSH_TEMPLATE_ID = "EDG-C100-WEL-00001.docx";
     public static final String CASE_DATA_FILE_C100 = "C100CaseData.json";
-    public static final String CASE_TEST_AUTHORISATION = "testAuth";
+    public static final String CASE_TEST_AUTHORIZATION = "testAuth";
+    public static final String CASE_DATA_C100_ID = "C100CaseData";
 
     private TestConstant() {
 
