5fb7b8eaf444e1f1a86862fd1c40a812215534d4
Adetola Opanuga <2109031@cognizant.com>
2022-06-22 17:37:14 +0100

    ISDB-250 Added Events and further services

--- src/test/java/uk/gov/hmcts/reform/cosapi/util/TestConstant.java
+++ src/test/java/uk/gov/hmcts/reform/cosapi/util/TestConstant.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.cosapi.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 public final class TestConstant {
 
@@ -9,7 +9,15 @@ public final class TestConstant {
     public static final String WELSH_TEMPLATE_ID = "EDG-C100-WEL-00001.docx";
     public static final String CASE_DATA_FILE_C100 = "C100CaseData.json";
     public static final String CASE_TEST_AUTHORIZATION = "testAuth";
-    public static final String CASE_DATA_C100_ID = "C100CaseData";
+    public static final String CASE_DATA_C100_ID = "C100";
+    public static final String JSON_FILE_TYPE = "json";
+    public static final String JSON_CONTENT_TYPE = "application/json";
+    public static final String RESPONSE_STATUS_SUCCESS = "Success";
+    public static final String TEST_URL = "TestUrl";
+    public static final String TEST_USER = "TestUser";
+    public static final String TEST_AUTHORIZATION_TOKEN = "TestToken";
+    public static final UUID TEST_CASE_DATA_FILE_UUID = UUID.randomUUID();
+    public static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(2022, 2, 22, 16, 21);
 
     private TestConstant() {
 
