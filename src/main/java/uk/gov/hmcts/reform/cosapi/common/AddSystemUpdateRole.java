3e6d4de6bab3e2b026b4a0a8e229fceda462423b
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-06-23 10:52:07 +0100

    Removed few roles not required for Version 1

--- src/main/java/uk/gov/hmcts/reform/cosapi/common/AddSystemUpdateRole.java
+++ src/main/java/uk/gov/hmcts/reform/cosapi/common/AddSystemUpdateRole.java
@@ -3,22 +3,11 @@ package uk.gov.hmcts.reform.cosapi.common;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class AddSystemUpdateRole {
-    private static final String ENVIRONMENT_AAT = "aat";
-
     public List<UserRole> addIfConfiguredForEnvironment(List<UserRole> userRoles) {
-        List<UserRole> existingRoles = new ArrayList<>(userRoles);
-        String environment = System.getenv().getOrDefault("ENVIRONMENT", null);
-
-        return existingRoles;
-    }
-
-    public boolean isEnvironmentAat() {
-        String environment = System.getenv().getOrDefault("ENVIRONMENT", null);
-        return null != environment && environment.equalsIgnoreCase(ENVIRONMENT_AAT);
+        return userRoles;
     }
 }
