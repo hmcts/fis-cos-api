d19a449b2f2da86f46c22905c997127dfb962c91
Rahul Sanjava <rahul.sanjava@HMCTS.NET>
2023-02-23 14:03:00 +0000

    feat: remove ccd config(ISDB-606)

--- bin/add-roles.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-scriptPath=$(dirname $(realpath $0))
-
-# Roles used during the CCD import
-${scriptPath}/add-ccd-role.sh "caseworker-privatelaw"
-${scriptPath}/add-ccd-role.sh "caseworker-privatelaw-solicitor"
-${scriptPath}/add-ccd-role.sh "caseworker-privatelaw-courtadmin"
-${scriptPath}/add-ccd-role.sh "citizen"
