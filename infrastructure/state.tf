9e049a0361aafee095f84cbab74a075899039ffa
yogendraU <66823759+yogendraU@users.noreply.github.com>
2023-03-14 16:45:10 +0000

    Update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,13 +1,3 @@
 terraform {
   backend "azurerm" {}
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.33.0"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
 }
